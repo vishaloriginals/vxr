b8e722717a443a928a29dacd140482c1c314a40a
vishaloriginals <119917644+vishaloriginals@users.noreply.github.com>
2024-08-22 19:37:05 +0530

    Update trial.js

--- trial.js
+++ trial.js
@@ -324,3 +324,14 @@ const birthDate = new Date(2005, 2, 9); // 9th March 2005
 
 // Display the age
 document.getElementById('age').textContent = getAge(birthDate);
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch((error) => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
