c5872f14e41f9e11db9cdc00252f4b7700712fc4
vishaloriginals <119917644+vishaloriginals@users.noreply.github.com>
2024-08-22 19:31:53 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/trial.css',
+        '/trial.js',
+        'weblogo192.svg',
+        'weblogo512.svg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
